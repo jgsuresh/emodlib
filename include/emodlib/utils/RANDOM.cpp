88d6d7eef13981c4f081fff730791e90f658787d
Edward Wenger <wenger.edward@gmail.com>
2023-02-17 10:46:16 -0800

    generalize Apple arm64 or Linux random libs with preprocessor directives

--- include/emodlib/utils/RANDOM.cpp
+++ include/emodlib/utils/RANDOM.cpp
@@ -6,11 +6,14 @@
 #ifndef WIN32
 #include <memory.h>    // memset
 #include <climits>     // UINT_MAX
+#if defined(__APPLE__) && defined(__arm64__)  // intended for Apple M1 hardware
 #include "sse2neon.h"
-// #include <emmintrin.h> // __m128i
-// #include <wmmintrin.h> // AES
-// #include <smmintrin.h> // _mm_insert_epi64
-// #include <tmmintrin.h> // _mm_shuffle_epi8
+#else
+#include <emmintrin.h> // __m128i
+#include <wmmintrin.h> // AES
+#include <smmintrin.h> // _mm_insert_epi64
+#include <tmmintrin.h> // _mm_shuffle_epi8
+#endif
 #endif
 
 #define PRNG_COUNT  (1<<20) // Let's start with ~1 million
